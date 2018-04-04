f60736d8cd65b73adcf51e91cf0c3329066ae914
Navis <nikavr@192.168.1.4>
2018-04-05 00:21:31 +0300

    go

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/jinzhu/gorm"
+	"project/database"
+)
 
 func main() {
+	db, err := database.Database{}
 	db, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
 	defer db.Close()
 }
